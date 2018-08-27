d48d34a2d28b5ffeb96573484df85e30be16d1e7
David Gomez <dg.dave@gmail.com>
2018-08-27 11:37:28 -0400

    Refactor defaults object

--- src/defaults.js
+++ src/defaults.js
@@ -29,35 +29,29 @@ export default {
    * sortByColumn {Number} [null] Index based number refering the column to be
    * used to sort the data, with or without a custom sorting function.
    */
-  // sortByColumn: null,
-  columnOptions: [
-    // {
-      /**
-       * headerLabel {String} [null] Label to use as the column header in this
-       * particular position. This will override the current header label if the
-       * header option is set to true. 
-       */
-      // headerLabel: null,
-      /**
-       * sortAscFunction {Function} [null] Custom compare function in the form
-       * (a, b) => a - b for data that requires special treatment in to be sorted.
-       */
-      // sortAscFunction: null,
-      /**
-       * sortDescFunction {Function} [null] Custom compare function in the form
-       * (a, b) => b - a for data that requires special treatment in to be sorted.
-       */
-      // sortDescFunction: null,
-      /**
-       * transformer {Function} [null] Function in the form a => a to be call on
-       * each table cell content for its corresponding column before render.
-       */
-      // transformer: null,
-      /**
-       * width {String} [null] Percentage of the total 100% table width to be
-       * used by each particular column
-       */
-      // width: null,
-    // },
-  ],
+  sortByColumn: null,
+  /**
+   * columnOptions {[Object]} [null] Array of objects containing the options for
+   * each column in the table (the objects may be empty). The number of objects
+   * in the array must be equal to the number of columns in the table.
+   *
+   * The available options are:
+   *
+   * $0.headerLabel {String} [null] Label to use as the column header in this
+   * particular position. This will override the current header label if the
+   * header option is set to true.
+   *
+   * $0.sortAscFunction {Function} [null] Custom compare function in the form
+   * (a, b) => a - b for data that requires special treatment in to be sorted.
+   *
+   * $0.sortDescFunction {Function} [null] Custom compare function in the form
+   * (a, b) => b - a for data that requires special treatment in to be sorted.
+   *
+   * $0.transformer {Function} [null] Function in the form a => a to be call on
+   * each table cell content for its corresponding column before render.
+   *
+   * $0.width {String} [null] Percentage of the total 100% table width to be
+   * used by each particular column
+   */
+  columnOptions: null,
 }
