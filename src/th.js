07aa4fa96bef98ab2db83888c03c856c95c61db5
David Gomez <dg.dave@gmail.com>
2018-08-28 09:03:57 -0400

    Fix code style

--- src/th.js
+++ src/th.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import identity from 'ramda/src/identity'
 
-const Th = ({ headerData, scope }) => (
-  <th scope={scope}>{headerData}</th>
-)
+const Th = ({ headerData, scope }) => <th scope={scope}>{headerData}</th>
 
 export default Th
