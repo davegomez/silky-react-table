69ce450c98faf4caf452720c5b6d37ac2eaf0de6
David Gomez <dg.dave@gmail.com>
2018-08-28 09:04:08 -0400

    Add Ramda functions

--- src/utils/set-scope.js
+++ src/utils/set-scope.js
@@ -1,5 +1,8 @@
 import always from 'ramda/src/always'
-import equals from 'ramda/src/equals'
+import complement from 'ramda/src/complement'
+import isNil from 'ramda/src/isNil'
 import when from 'ramda/src/when'
 
-export default scope => when(equals(true), always(scope))
+const setScope = scope => when(complement(isNil), always(scope))
+
+export default setScope
